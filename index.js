4d445c52b70d79a571da982476819373097dac09
Davian Morris <davianmorris35@gmail.com>
2023-07-07 00:11:38 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+if (mainElement) {
+  mainElement.remove();
+}
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+const yourName = 'Davian Morris'; 
+newHeader.textContent = `${yourName} is the champion`;
